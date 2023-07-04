4c609211a79aa08ca1a487e218d3481a251875b9
AkmalKurbanov <250296a@gmail.com>
2023-07-04 18:14:53 +0600

    calculator

--- app/js/calculator-tabs.js
+++ app/js/calculator-tabs.js
@@ -1,47 +1,63 @@
-$('.calculator__calculator-tabs-wrap').on('click', function() {
-  $(this).toggleClass('open');
-  
+// $('.calculator__calculator-tabs-wrap').on('click', function() {
+//   $(this).toggleClass('open');
+
+// });
+// $('.calculator__calculator-tab').on('click', function() {
+//   let svg = $(this).find('.icon svg').clone();
+//   let nameTab = $(this).find('span').text();
+//   $('.calculator__calculator-tabs-selected').find('.calculator__calculator-tabs-selected-icon').html(svg)
+//   $('.calculator__calculator-tabs-selected span').text(nameTab);
+//   console.log(nameTab)
+// });
+
+
+
+
+// $(document).ready(function() { 
+
+// 	(function ($) { 
+// 		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+
+// 		$('.tab ul.tabs li a').click(function (g) { 
+// 			var tab = $(this).closest('.tab'), 
+// 				index = $(this).closest('li').index();
+
+// 			tab.find('ul.tabs > li').removeClass('current');
+// 			$(this).closest('li').addClass('current');
+
+// 			tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').hide();
+// 			tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').fadeIn();
+
+// 			g.preventDefault();
+// 		} );
+// 	})(jQuery);
+
+// });
+
+
+if (window.matchMedia('(min-width: 992px)').matches) {
+	let calculatorWidth = $('.calculator__calculator').outerWidth();
+	let distance1 = $("#container").offset().left;
+	let distance2 = $(".calculator__calculator form").offset().left;
+	let distance3 = distance1 - distance2 + 15;
+	$('.calculator__calculator-bottom').css('width', calculatorWidth);
+	$('.calculator__calculator-bottom').css('margin-left', - distance3);
+}
+
+
+$(window).on('resize', function () {
+	let calculatorWidth = $('.calculator__calculator').outerWidth();
+	let distance1 = $("#container").offset().left;
+	let distance2 = $(".calculator__calculator form").offset().left;
+	let distance3 = distance1 - distance2 + 15;
+	$('.calculator__calculator-bottom').css('width', calculatorWidth);
+	$('.calculator__calculator-bottom').css('margin-left', - distance3);
+
 });
-$('.calculator__calculator-tab').on('click', function() {
-  let svg = $(this).find('.icon svg').clone();
-  let nameTab = $(this).find('span').text();
-  $('.calculator__calculator-tabs-selected').find('.calculator__calculator-tabs-selected-icon').html(svg)
-  $('.calculator__calculator-tabs-selected span').text(nameTab);
-  console.log(nameTab)
+
+$('.calculator__calculator-tab-wrap .calculator__calculator-tab').on('click', function () {
+	$(this).parents('.calculator__calculator-tab-wrap').addClass('open');
+	$('.calculator__calculator-tab-wrap').not($(this).parents('.calculator__calculator-tab-wrap')).removeClass('open');
 });
 
 
-
-
-$(document).ready(function() { 
-
-	(function ($) { 
-		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
-		
-		$('.tab ul.tabs li a').click(function (g) { 
-			var tab = $(this).closest('.tab'), 
-				index = $(this).closest('li').index();
-			
-			tab.find('ul.tabs > li').removeClass('current');
-			$(this).closest('li').addClass('current');
-			
-			tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').hide();
-			tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').fadeIn();
-			
-			g.preventDefault();
-		} );
-	})(jQuery);
-
-});
-
-
-
-
-
-$('.calculator-mobile .calculator__calculator-tab').on('click', function(e){
-	e.preventDefault()	;
-	$(this).next().slideToggle();
-	$(this).toggleClass('open');
-	$('.calculator__calculator-calculation').not($(this).parents('.calculator-mobile__item').find('.calculator__calculator-calculation')).slideUp();
-	$('.calculator__calculator-tab').not(this).removeClass('open');
-});
